66d028409e3ea7c678ae8c459b53e0013454705b
Kayler Renslow <kaylerrenslow@gmail.com>
2016-03-16 15:18:36 -0500

    >going to attempt to scrap the sqf grammar and create something better... again

---
+++ src/com/kaylerrenslow/plugin/lang/sqf/SQFAnnotator.java
@@ -0,0 +1,29 @@
+package com.kaylerrenslow.plugin.lang.sqf;
+
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiErrorElement;
+import com.kaylerrenslow.plugin.lang.sqf.psi.SQFIfStatement;
+import com.kaylerrenslow.plugin.lang.sqf.psi.SQFVisitor;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by Kayler on 03/15/2016.
+ */
+public class SQFAnnotator implements Annotator{
+
+	@Override
+	public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+		if(element instanceof PsiErrorElement){
+
+		}
+		element.accept(new SQFVisitor(){
+			@Override
+			public void visitIfStatement(@NotNull SQFIfStatement o) {
+				super.visitIfStatement(o);
+				System.out.println("IFFF");
+			}
+		});
+	}
+}
