5fcf98fa25dc1509f7861979a89e045a363ff7c4
Kayler Renslow <kaylerrenslow@gmail.com>
2016-05-22 14:45:37 -0500

    moved dm to own repo

--- src/com/kaylerrenslow/armaDialogCreator/gui/fx/control/treeView/CellType.java
+++
@@ -1,8 +0,0 @@
-package com.kaylerrenslow.armaDialogCreator.gui.fx.control.treeView;
-
-/**
- Created by Kayler on 05/15/2016.
- */
-public enum CellType {
-	LEAF, FOLDER, COMPOSITE
-}
--- src/com/kaylerrenslow/armaDialogCreator/gui/fx/control/treeView/ITreeCellSelectionUpdate.java
+++
@@ -1,9 +0,0 @@
-package com.kaylerrenslow.armaDialogCreator.gui.fx.control.treeView;
-
-/**
- Created by Kayler on 05/15/2016.
- */
-public interface ITreeCellSelectionUpdate {
-
-	void selectionUpdate(CellType selectedType, CellType parentCellType);
-}
