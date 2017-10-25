a6507e73701bdc7adc55ecac4da0e86be07886aa
Kayler Renslow <kaylerrenslow@gmail.com>
2017-10-24 22:46:57 -0500

    made a lil more progress on arma tools dir settings dialog

--- src/com/kaylerrenslow/armaplugin/ArmaPluginProjectConfigurable.java
+++ src/com/kaylerrenslow/armaplugin/ArmaPluginProjectConfigurable.java
@@ -6,16 +6,15 @@ import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.io.File;
-import java.util.ResourceBundle;
 
 /**
  * @author Kayler
  * @since 09/28/2017
  */
 public class ArmaPluginProjectConfigurable implements Configurable {
-	private final ResourceBundle bundle = ArmaPlugin.getPluginBundle();
+
 	private final JLabel lblCurrentDirPath = new JLabel();
+	private final ArmaPluginSettingsForm form = new ArmaPluginSettingsForm();
 
 	@Nls
 	@Override
@@ -26,34 +25,7 @@ public class ArmaPluginProjectConfigurable implements Configurable {
 	@Nullable
 	@Override
 	public JComponent createComponent() {
-		return new ArmaPluginSettingsForm().getPanelRoot();
-//		JPanel root = new JPanel();
-//		root.setLayout(new BoxLayout(root, BoxLayout.Y_AXIS));
-//
-//		JLabel lblAbout = new JLabel(bundle.getString("Dialog.ArmaToolsConfig.about_label"));
-//		lblAbout.setFont(lblAbout.getFont().deriveFont(Font.PLAIN, 15));
-//		root.add(lblAbout);
-//		lblAbout.setHorizontalAlignment(SwingConstants.LEFT);
-//
-//		{ //view and set the current arma tools directory
-//			JPanel panel = new JPanel();
-//			panel.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 5));
-//			panel.add(new JLabel(bundle.getString("Dialog.ArmaToolsConfig.current-directory")));
-//			panel.add(lblCurrentDirPath);
-//
-//			updateCurrentDirPathLabel();
-//
-//			root.add(panel);
-//		}
-//
-//		return root;
-	}
-
-	private void updateCurrentDirPathLabel() {
-		File armaTools = ArmaPluginUserData.getInstance().getArmaToolsDirectory();
-		lblCurrentDirPath.setText(armaTools == null ?
-				bundle.getString("Dialog.ArmaToolsConfig.not-set") : armaTools.getAbsolutePath()
-		);
+		return form.getPanelRoot();
 	}
 
 	@Override
--- src/com/kaylerrenslow/armaplugin/ArmaPluginSettingsForm.java
+++ src/com/kaylerrenslow/armaplugin/ArmaPluginSettingsForm.java
@@ -1,8 +1,11 @@
 package com.kaylerrenslow.armaplugin;
 
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import javafx.stage.DirectoryChooser;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.io.File;
 
 /**
  * @author Kayler
@@ -11,12 +14,33 @@ import javax.swing.*;
 public class ArmaPluginSettingsForm {
 	private JPanel panelRoot;
 	private JPanel panelArmaToolsCfg;
-	private JTextField tfArmaToolsDir;
-	private JButton btnLocateArmaTools;
 	private JButton btnTestDir;
+	private JPanel panelForTfArmaToolsDir;
 
 	@NotNull
 	public JPanel getPanelRoot() {
 		return panelRoot;
 	}
+
+	private void createUIComponents() {
+		JTextField tfArmaToolsDir = new JTextField(20);
+		{
+			panelForTfArmaToolsDir = new TextFieldWithBrowseButton(tfArmaToolsDir, e -> {
+				DirectoryChooser dc = new DirectoryChooser();
+				File file = dc.showDialog(null);
+				if (file == null) {
+					return;
+				}
+				tfArmaToolsDir.setText(file.getAbsolutePath());
+			});
+		}
+		{
+			btnTestDir.addActionListener(e -> {
+				boolean validA3ToolsDirectory = ArmaTools.isValidA3ToolsDirectory(new File(tfArmaToolsDir.getText()));
+				
+			});
+		}
+	}
+
+
 }
