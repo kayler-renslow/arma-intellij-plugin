b73ec2124b84d2556d13445b2d0ed56ee1f017d8
Kayler Renslow <kaylerrenslow@gmail.com>
2017-11-09 00:54:42 -0600

    making slooow progress on addons settings form

--- src/com/kaylerrenslow/armaplugin/settings/ArmaAddonsSettingsForm.java
+++ src/com/kaylerrenslow/armaplugin/settings/ArmaAddonsSettingsForm.java
@@ -1,14 +1,14 @@
 package com.kaylerrenslow.armaplugin.settings;
 
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
-import com.intellij.ui.JBSplitter;
-import com.intellij.ui.components.JBList;
+import com.intellij.ui.ColoredTreeCellRenderer;
 import com.intellij.ui.treeStructure.Tree;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import javax.swing.JComponent;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.util.ResourceBundle;
 
 /**
  * @author Kayler
@@ -17,22 +17,59 @@ import javax.swing.JTextField;
 public class ArmaAddonsSettingsForm {
 	private JPanel panelRoot;
 	private TextFieldWithBrowseButton tfWithBrowseReferenceDirectory;
-	private JBSplitter splitterAddonsRoots;
+	private Tree treeAddonsRoots;
+	private ResourceBundle bundle;
+	private DefaultMutableTreeNode treeAddonsRoots_rootNode;
 
 	private void createUIComponents() {
+		bundle = ResourceBundle.getBundle("com.kaylerrenslow.armaplugin.ProjectSettingsBundle");
+
 		tfWithBrowseReferenceDirectory = new TextFieldWithBrowseButton(new JTextField(40));
 
-		{
-			splitterAddonsRoots = new JBSplitter(false);
-			Tree tree = new Tree();
-			splitterAddonsRoots.setFirstComponent(tree);
-			splitterAddonsRoots.setSecondComponent(new JBList<>());
-		}
-
+		treeAddonsRoots_rootNode = new RootTreeNode();
+		treeAddonsRoots = new Tree(treeAddonsRoots_rootNode);
+		treeAddonsRoots.setCellRenderer(new MyColoredTreeCellRenderer());
+		treeAddonsRoots.setHoldSize(true);
+		treeAddonsRoots.addTreeSelectionListener(e -> {
+			System.out.println("ArmaAddonsSettingsForm.createUIComponents e=" + e);
+		});
 	}
 
 	@NotNull
 	public JComponent getPanelRoot() {
 		return panelRoot;
+	}
+
+	private class ArmaAddonTreeNode extends DefaultMutableTreeNode {
+
+	}
+
+	private class ArmaAddonRootTreeNode extends DefaultMutableTreeNode {
+
+	}
+
+	private class RootTreeNode extends DefaultMutableTreeNode {
+
+		public RootTreeNode() {
+			super(bundle.getString("addon-roots"), true);
+		}
+	}
+
+
+	private abstract class TreeNodeValue {
+		@Nullable
+		public Icon getIcon() {
+			return null;
+		}
+	}
+
+	private class MyColoredTreeCellRenderer extends ColoredTreeCellRenderer {
+		@Override
+		public void customizeCellRenderer(@NotNull JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+			if (value instanceof TreeNodeValue) {
+				TreeNodeValue treeNodeValue = ((TreeNodeValue) value);
+				setIcon(treeNodeValue.getIcon());
+			}
+		}
 	}
 }
